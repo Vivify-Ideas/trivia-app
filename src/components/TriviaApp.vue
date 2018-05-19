8efd8eef28fcd5343f9db8c9d2025c6a74621f06
Nikola Spalevic <nikola.s@vivifyideas.com>
2018-05-19 16:13:41 +0200

    Commit 7

--- src/components/TriviaApp.vue
+++ src/components/TriviaApp.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <select
-      class="custom-select"
+      class="custom-select mb-3 mt-3"
       size="10"
       @change="onCategoryChanged"
     >
@@ -12,10 +12,18 @@
         {{ triviaCategory.title }}
       </option>
     </select>
+
+    <input
+      type="text"
+      class="form-control mb-3"
+      placeholder="Filter Trivias"
+      v-model="filterTriviaTerm"
+    />
+
     <div class="accordion">
       <div
         class="card"
-        v-for="trivia in trivias"
+        v-for="trivia in currentlyVisibleTrivias"
         :key="trivia.id"
         @click="toggleTrivia(trivia.id)"
       >
@@ -35,24 +43,53 @@
         </div>
       </div>
     </div>
+
+    <custom-paginator
+      class="mb-3 mt-3"
+      :number-of-pages="numberOfPages"
+      :current-page="currentPage"
+      @selected-page="setCurrentPage"
+    />
   </div>
 </template>
 
 <script>
+import CustomPaginator from './CustomPaginator.vue'
 import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'TriviaApp',
+  components: {
+    CustomPaginator
+  },
   data() {
     return {
-      selectedTriviasIds: []
+      selectedTriviasIds: [],
+      filterTriviaTerm: '',
+      currentPage: 1
     };
   },
   computed: {
     ...mapGetters({
       trivias: 'getTrivias',
       triviaCategories: 'getTriviaCategories'
-    })
+    }),
+    filteredTrivias() {
+      return this.trivias.filter((trivia) => {
+        return trivia.question.toLowerCase()
+          .indexOf(this.filterTriviaTerm.toLowerCase()) > -1;
+      })
+    },
+    currentlyVisibleTrivias() {
+      let bottomLimit = (this.currentPage - 1) * 5;
+      let topLimit = bottomLimit + 5;
+      return this.filteredTrivias.filter((trivia, index) => {
+        return index >= bottomLimit && index < topLimit;
+      })
+    },
+    numberOfPages() {
+      return Math.ceil(this.filteredTrivias.length / 5);
+    }
   },
   methods: {
     ...mapActions([
@@ -72,6 +109,9 @@ export default {
     },
     onCategoryChanged(event) {
       this.fetchTrivias(event.target.value);
+    },
+    setCurrentPage(page) {
+      this.currentPage = page;
     }
   },
   created() {
